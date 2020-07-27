0c276429287292519a1e99adcefc0af84108dd3c
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-07-27 12:29:35 -0400

    ✨ Add newRuleByIdentifier to global style

--- js/globalStyle.js
+++ js/globalStyle.js
@@ -16,6 +16,33 @@ class GlobalStyle {
         styles.innerHTML += "}";
     }
     /**
+     * Add a new css rule by an identifier.
+     * The difference between this and newClass 
+     * is that newClass must be class.
+     * This method allows for things based on element type
+     * and id (although id should not be used as multiple instances of a window may cause issues)
+     * @param {String} identifierName - The name of the identifier. e.g. 'div' or #abc
+     * @param  {...String} args 
+     */
+    static newRuleByIdentifier(identifierName, ...args) {
+        let styles = document.getElementById("global-style");
+        styles.innerHTML += "\n"+identifierName+" {\n";
+        this.addArgumentsAsRules(args);
+        styles.innerHTML += "}";
+    }
+    /**
+     * Add arguments as rules
+     * @param {Array} args - The arguments from the ...args parameter
+     */
+    addArgumentsAsRules(args) {
+        args.forEach((arg)=>{
+            styles.innerHTML += "\t"+arg+"\n";
+            if(arg.indexOf(";") == -1) {
+                console.error("Class "+ruleName+" is missing a semicolon for argument: "+arg+".");
+            }
+        });
+    }
+    /**
      * Add raw css to the global style.
      * @param {String} css - The css to be added.
      */

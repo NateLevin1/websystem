04ba0a5fb38d7e60fc76d226ad1897b797307e82
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-08-31 20:15:13 -0400

    ⚡️ Improve performance

--- backend/php/set.php
+++ backend/php/set.php
@@ -16,29 +16,15 @@ if ($payload) { // if token is valid
     $id = $payload['sub'];
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
-
-    $getSql = "SELECT 1 FROM folders WHERE id='{$id}'";
-    $exists = $conn->query($getSql);
-    if ($exists->num_rows > 0) {
-        $update = "UPDATE folders SET json='{$_POST['json']}' WHERE id='{$id}'";
-
-        if ($conn->query($update) === TRUE) {
-            echo "Record updated successfully";
-        } else {
-            echo "Error updating record: " . $conn->error . ". Did you update the username, password, and DB names?";
-            http_response_code(400);
-        }
-    } else {
-        $sql = "INSERT INTO folders (id, json)
+    $sql = "INSERT or REPLACE INTO folders (id, json)
     VALUES ('{$id}', '{$_POST['json']}')";
 
-        if ($conn->query($sql) === TRUE) {
-            echo "New record created successfully";
-        } else {
-            echo "Error: " . $sql . "<br>" . $conn->error . ". Did you update the username, password, and DB names?";
-            http_response_code(400);
-        }   
-    }
+    if ($conn->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error . ". Did you update the username, password, and DB names?";
+        http_response_code(400);
+    }   
 
     $conn->close();
 } else {

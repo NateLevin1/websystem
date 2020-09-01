13c227afb7c6b82fe5e605c847e86d5a27848674
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-09-01 10:41:07 -0400

    🔒 Add security to get.php

--- backend/php/get.php
+++ backend/php/get.php
@@ -1,30 +1,46 @@
 <?php
+require_once __DIR__.'/vendor/autoload.php';
+
 $servername = "localhost";
 $username = "USERNAME";
 $password = "PASSWORD";
 $dbname = "DBNAME";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-$sql = "SELECT json FROM folders WHERE id={$_GET['id']}";
-
-
-// Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-  http_response_code(400);
-} 
-
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-    echo $row["json"];
-  }
+if(gettype($_GET['id_token']) != "string") {
+  echo "Invalid Token Type.";
 }
 
+$client = new Google_Client(['client_id' => "321811210964-jcb9c7kbtrf53ghv2ugvj82a47shm5hg.apps.googleusercontent.com"]);
+$payload = $client->verifyIdToken($_GET['id_token']);
 
-$conn->close();
+if ($payload) { // if token is valid
+  $id = $payload['sub'];
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  $sql = "SELECT json FROM folders WHERE id={$id}";
+
+
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    http_response_code(400);
+  } 
+
+  $result = $conn->query($sql);
+
+  if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+      echo $row["json"];
+    }
+  }
+
+
+  $conn->close();
+} else {
+  echo "Error: Invalid ID Token";
+  http_response_code(400);
+}
 ?>

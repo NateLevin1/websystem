c6dfdc73201c635b5f41b1bfd05beeccca7965bc
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-09-02 10:03:44 -0400

    🐛 Fix bug

--- js/boot.js
+++ js/boot.js
@@ -123,18 +123,20 @@ function* foldersSignIn() {
     if(!firstLogin) {
         yield;
     }
-    fetch("https://www.websystem.io/backend/php/get.php?id_token="+googleProfile["id_token"])
-    .then((response)=>{
-        return response.json();
-    })
-    .then(async (json)=>{
-        folders = json.folders;
-        let f64 = json.files;
-        for(let key in f64) {
-            files[key] = await (await fetch(f64[key])).blob(); // this is the reverse of what happens when it is converted to b64
-        }
-        readySystem();
-    });
+    if(!firstLogin) {
+        fetch("https://www.websystem.io/backend/php/get.php?id_token="+googleProfile["id_token"])
+        .then((response)=>{
+            return response.json();
+        })
+        .then(async (json)=>{
+            folders = json.folders;
+            let f64 = json.files;
+            for(let key in f64) {
+                files[key] = await (await fetch(f64[key])).blob(); // this is the reverse of what happens when it is converted to b64
+            }
+            readySystem();
+        });
+    }
 }
 
 const fsi = foldersSignIn();

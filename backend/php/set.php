2f05feeab53687e24325fec1bafca569465e0b3a
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-09-02 17:09:21 -0400

    🐛 Revert changes that broke backend

--- backend/php/set.php
+++ backend/php/set.php
@@ -15,20 +15,6 @@ $payload = $client->verifyIdToken($_POST['id_token']);
 if ($payload) { // if token is valid
     $id = $payload['sub'];
 
-    $c = new mysqli($servername, $username, $password, $dbname);
-    // set max_allowed_packet
-    $setPacket = "SET GLOBAL max_allowed_packet=1073741824";
-    if ($c->query($setPacket) === TRUE) {
-        echo "Max packet global updated successfully";
-    } else {
-        echo "Error updating max packet global: " . $c->error . ".";
-        http_response_code(500);
-    }
-    $c->close();
-
-
-    
-    
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
 

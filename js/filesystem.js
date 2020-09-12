80d743e2070c9a9d3a02f123fc170deb88328556
UltimateProGrammer <63370104+UltimatePro-Grammer@users.noreply.github.com>
2020-09-12 12:44:43 -0400

    🐛 Fix unable to save data to server

--- js/filesystem.js
+++ js/filesystem.js
@@ -369,8 +369,7 @@ class FileSystem {
                         files64[key] = files[key];
                     }
                 }
-                var p = Object.keys(files64);
-                if(Object.keys(oldFiles64).every(function (i) { return p.indexOf(i) !== -1; }) && p.every(function (i) { return objectEquals(files64[i], oldFiles64[i]); })) {
+                if(objectEquals(oldFiles64, files64)) {
                     sendDataToServer(showAlert, false);
                 } else {
                     sendDataToServer(showAlert, true);
@@ -429,3 +428,30 @@ var serverUpdateTimeout;
 var fileSystemUpdate = new Event("file-system-update"); // dispatched on the document whenever there is a change to the filesystem. The 
 
 var files64 = {};
+
+// from https://stackoverflow.com/a/16788517/
+const objectEquals = (x, y)=>{
+    'use strict';
+
+    if (x === null || x === undefined || y === null || y === undefined) { return x === y; }
+    // after this just checking type of one would be enough
+    if (x.constructor !== y.constructor) { return false; }
+    // if they are functions, they should exactly refer to same one (because of closures)
+    if (x instanceof Function) { return x === y; }
+    // if they are regexps, they should exactly refer to same one (it is hard to better equality check on current ES)
+    if (x instanceof RegExp) { return x === y; }
+    if (x === y || x.valueOf() === y.valueOf()) { return true; }
+    if (Array.isArray(x) && x.length !== y.length) { return false; }
+
+    // if they are dates, they must had equal valueOf
+    if (x instanceof Date) { return false; }
+
+    // if they are strictly equal, they both need to be object at least
+    if (!(x instanceof Object)) { return false; }
+    if (!(y instanceof Object)) { return false; }
+
+    // recursive object equality check
+    var p = Object.keys(x);
+    return Object.keys(y).every(function (i) { return p.indexOf(i) !== -1; }) &&
+        p.every(function (i) { return objectEquals(x[i], y[i]); });
+}
